9a6f22afa57e0c7a6c245dd449d152a568cdf6fb
Sait CİHANER <saitcihaner@hotmail.com>
2022-03-23 19:27:21 +0300

    Initial Commit

---
+++ AlomofireUse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlomofireUse
+//
+//  Created by Sait Cihaner on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
